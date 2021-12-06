77311b951e53a35555724575e0ab5c1bf29cc366
Padyaa <Padya@MacBook-Andrej.local>
2021-12-06 14:41:47 +0300

    autotest

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForNotRegistered() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
